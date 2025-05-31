3a2816a6bd91a49fc25c244b402f4edeedb2d280
iiYahya56 <73726392+iiYahya56@users.noreply.github.com>
2025-06-01 03:59:36 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from honeynet import web_server, logger, config
+from honeynet import web_server, ssh_emulator, logger, config
+from threading import Thread
 
 if __name__ == '__main__':
     logger.init_db()
-    web_server.app.run(host=config.HOST, port=config.PORT)
+    
+    # Run Flask app in a thread
+    t1 = Thread(target=web_server.app.run, kwargs={
+        'host': config.HOST,
+        'port': config.PORT
+    })
+    t1.start()
+
+    # Run SSH emulator
+    ssh_emulator.run_ssh_emulator()
